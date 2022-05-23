25d2868b651590e9c005eb7b0839b914cb318786
Jasper Pan <105698516+jasperpan8@users.noreply.github.com>
2022-05-22 23:38:40 -0500

    Update GetEmployeeDemo.java

--- src/GetEmployeeDemo.java
+++ src/GetEmployeeDemo.java
@@ -9,22 +9,32 @@ import org.hibernate.cfg.Configuration;
 
 public class GetEmployeeDemo {
     public static void main(String[] args) {
+        
+        // create session factory
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .addAnnotatedClass(Department.class)
                 .addAnnotatedClass(Department_Employee.class)
                 .buildSessionFactory();
+        
+        // create session
         Session session = factory.getCurrentSession();
 
         try{
+            // start a transaction
             session.beginTransaction();
+            
+            // get the employee object
             int theId = 6;
             Employee e = session.get(Employee.class, theId);
             System.out.println(e);
             System.out.println("Employee with Id"+ theId +" is " + e.getName());
+            
+            // commit transaction
             session.getTransaction().commit();
             System.out.println("Done!");
+            
         }catch (Exception e){
             e.printStackTrace();
         } finally {
