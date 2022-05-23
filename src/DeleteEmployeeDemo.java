cda7758cbc443b3ff2ebc620eb83a19527fc75b4
Jasper Pan <105698516+jasperpan8@users.noreply.github.com>
2022-05-22 23:37:22 -0500

    Update DeleteEmployeeDemo.java

--- src/DeleteEmployeeDemo.java
+++ src/DeleteEmployeeDemo.java
@@ -10,22 +10,33 @@ import org.hibernate.cfg.Configuration;
 public class DeleteEmployeeDemo {
 
     public static void main(String[] args) {
+        // create session factory
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .addAnnotatedClass(Department.class)
                 .addAnnotatedClass(Department_Employee.class)
                 .buildSessionFactory();
+        
+        // create session
         Session session = factory.getCurrentSession();
 
         try{
+            // start a transaction
             session.beginTransaction();
+            
+            // get the employee object
             int theId = 6;
             Employee e = session.get(Employee.class, theId);
             System.out.println("Deleting Employee " +e);
+            
+            // delete the employee Id 6
             session.delete(e);
+            
+            // commit transaction
             session.getTransaction().commit();
             System.out.println("Done!");
+            
         }catch (Exception e){
             e.printStackTrace();
         }
