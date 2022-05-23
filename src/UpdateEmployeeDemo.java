18f3baae81059e549a53bd82bca487ecf3fb48a7
Jasper Pan <105698516+jasperpan8@users.noreply.github.com>
2022-05-22 23:40:22 -0500

    Update UpdateEmployeeDemo.java

--- src/UpdateEmployeeDemo.java
+++ src/UpdateEmployeeDemo.java
@@ -9,22 +9,34 @@ import org.hibernate.cfg.Configuration;
 
 public class UpdateEmployeeDemo {
     public static void main(String[] args) {
+        // create session factory
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
                 .addAnnotatedClass(Employee.class)
                 .addAnnotatedClass(Department.class)
                 .addAnnotatedClass(Department_Employee.class)
                 .buildSessionFactory();
+        
+        // create session
         Session session = factory.getCurrentSession();
 
         try{
+            // start a transaction
             session.beginTransaction();
+            
+            // get employee object
             Employee e = new Employee();
             e.setName("Jay");
             e.setId(2);
+            
+            // update the employee object
             session.saveOrUpdate(e);
             System.out.println(e);
+            
+            // commit transaction
             session.getTransaction().commit();
+            System.out.println("Done!");
+            
         }finally {
             session.close();
             factory.close();
